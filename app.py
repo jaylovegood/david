e15bf9eef257c301638d54b494846eba19f90b7b
홍지수 <picokosmos@c3r4s6.codyssey.kr>
2025-08-24 20:32:44 +0900

    defined separate methods for GET and POST

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import os
+import base64
 from io import BytesIO
 from gtts import gTTS
 
 DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/", methods=["GET"])
+def get_home():
+    return render_template("index.html")
 
-    text = "Hello, DevOps"
+@app.route("/", methods=["POST"])
+def post_home():
 
-    lang = request.args.get('lang', DEFAULT_LANG)
-    fp = BytesIO()
-    gTTS(text, "com", lang).write_to_fp(fp)
+    text = request.form.get('input_text')
+    lang = request.form.get('lang', DEFAULT_LANG)
 
-    return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
+    if not text:
+        return render_template("index.html", error="텍스트를 입력하세요.")
+
+    try:
+        
+        fp = BytesIO()
+        gTTS(text, "com", lang).write_to_fp(fp)
+
+        fp.seek(0)
+
+        audio_b64 = base64.b64encode(fp.read()).decode("ascii")
+        return render_template("index.html", audio=audio_b64)
+
+    except Exception as e:
+        return render_template("index.html", error=f"음성 생성 실패: {e}")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080)
